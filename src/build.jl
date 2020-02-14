6f4aa552c478f8a52674464d4d4f9e0dcfcebe5c
matsueushi <matsueushi@gmail.com>
2020-02-13 23:21:17 -0500

    build

--- src/build.jl
+++ src/build.jl
@@ -1,6 +1,16 @@
+# Original: src/notebooks.jl of https://github.com/MikeInnes/diff-zoo
+# Copyright (c) 2019 Mike J Innes
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
+root = joinpath(@__DIR__, "..")
+using Pkg; Pkg.activate(root)
 using Literate
 
-for x in ["src/ch1-computer-arithmetic.jl"]
+src = joinpath(root, "src")
+out = joinpath(root, "notebook")
+
+for x in ["ch1-computer-arithmetic.jl"]
     # Literate.markdown(x, "./markdown"; documenter=true)
-    Literate.notebook(x, "./notebook"; documenter=true)
+    Literate.notebook(joinpath(src, x), out; documenter=true)
 end
